eea8de7fac5860f94a2982b4fa5f16263313c159
Julie K <julieskim160@gmail.com>
2018-03-27 17:20:52 -0700

    jeff's request

---
+++ scripts/store.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const store = (function() {
+  const addItem = function(item) {
+    this.items.push(item);
+  };
+
+  [
+    {
+      id: 1,
+      title: 'google maps',
+      link: 'https://www.google.com/maps',
+      description: 'mapquest but better',
+      rating: '5',
+      expand: false
+    }
+  ];
+
+
+
+});
